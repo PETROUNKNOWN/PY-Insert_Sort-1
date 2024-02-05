352a54b3ef97e3bf9ed4c73f2608c0070a3ea898
peet <112712989+PETROUNKNOWN@users.noreply.github.com>
2024-02-05 19:48:27 +0300

    First Sorting algo.

---
+++ sort1.py
@@ -0,0 +1,14 @@
+#Python Insertion Sort
+
+def instertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while arr[j -1] > arr[j] and j > 0:
+            arr[j - 1], arr[j] = arr[j], arr[j - 1]
+            j -= 1
+            
+arr  = [2, 6, 5, 1, 3, 4]
+instertion_sort(arr)
+print(arr)
+
+#result -> [1, 2, 3, 4, 5, 6]
